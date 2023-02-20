9a2487840e9dc4c7d841951edb03d5a349f47ffe
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-02-20 21:58:35 +0530

    fixing bug

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-
-
-
-
-// const plays = () => {
-//     console.log("hi");
-//     let song = document.querySelector('#music')
-//     console.log(song.value);
-    
-//     let player = document.querySelector('#player')
-//     player.innerHTML = `
-//     <audio controls>
-//     <source src="${song.value}" >
-//     </audio>`
-    
-
-// }
+let a
+let b
 
 function player() {
-    readFile(document.querySelector('#music').files)
+    if (document.querySelector('#music').files.length > 0) {
+        readFile(document.querySelector('#music').files)
+        const playButton = document.querySelector('.button')
+        playButton.innerHTML = `<button class="Pause" onclick="stop()">Stop</button>`
+    }
+}
+
+function stop() {
+    window.location.reload();
 }
 
 function readFile(files) {
-    console.log('files');
-    console.log(files);
     var fileReader = new FileReader();
-        fileReader.readAsArrayBuffer(files[0]);
-        fileReader.onload = function(e) {
-            console.log('e.target.result');
-            playAudioFile(e.target.result);
-            console.log(e.target.result);
-            console.log(("Filename: '" + files[0].name + "'"), ( "(" + ((Math.floor(files[0].size/1024/1024*100))/100) + " MB)" ));
-        }
+    fileReader.readAsArrayBuffer(files[0]);
+    fileReader.onload = function(e) {
+        playAudioFile(e.target.result);
+        const title = document.querySelector('.titles')
+        title.innerHTML = `<h1> Playing :  ${files[0].name}  </h1>`
+    }
 }
+
 function playAudioFile(file) {
     var context = new window.AudioContext();
-        context.decodeAudioData(file, function(buffer) {
-            var source = context.createBufferSource();
-                source.buffer = buffer;
-                source.loop = false;
-                source.connect(context.destination);
-                source.start(0); 
-        });
+    a = file;
+    context.decodeAudioData(file, function(buffer) {
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        source.loop = false;
+        source.connect(context.destination);
+        source.start(0); 
+    });
+    b = context
 }
